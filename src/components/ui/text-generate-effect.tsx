4bc27a2928bc0893c409fb01ff7bc614551bdaea
cardosofiles <cardosofiles@outlook.com>
2025-03-26 04:02:30 -0300

    fixing deployment errors on vercel

--- src/components/ui/text-generate-effect.tsx
+++ src/components/ui/text-generate-effect.tsx
@@ -19,21 +19,18 @@ export const TextGenerateEffect = ({
   const wordsArray = words.split(" ");
 
   useEffect(() => {
-    // Certificando-se de que 'scope.current' é uma referência válida
-    if (scope.current) {
-      animate(
-        scope.current.children, // Aplica a animação aos filhos do 'scope'
-        {
-          opacity: 1,
-          filter: filter ? "blur(0px)" : "none",
-        },
-        {
-          duration: duration,
-          delay: stagger(0.2),
-        }
-      );
-    }
-  }, [animate, filter, duration, scope.current]); // Dependências ajustadas para garantir que a animação seja chamada corretamente
+    animate(
+      "span",
+      {
+        opacity: 1,
+        filter: filter ? "blur(0px)" : "none",
+      },
+      {
+        duration: duration ? duration : 1,
+        delay: stagger(0.2),
+      }
+    );
+  });
 
   const renderWords = () => {
     return (
@@ -41,7 +38,8 @@ export const TextGenerateEffect = ({
         {wordsArray.map((word, idx) => {
           return (
             <motion.span
-              key={word}
+              // biome-ignore lint/suspicious/noArrayIndexKey: <explanation>
+              key={word + idx}
               className="dark:text-white text-black opacity-0"
               style={{
                 filter: filter ? "blur(10px)" : "none",
